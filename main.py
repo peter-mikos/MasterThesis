6a4d253548e95500197cf26ded4a0b7311e15d7a
Peter Mikos <19peter99222@gmail.com>
2024-05-11 14:09:34 +0200

    Main Results

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ from keras.layers import Input, Dense, Subtract, Multiply, Add, Dot, Concatenate
 from keras.models import Sequential, Model
 from SABR import SABR_model as path
 from DeepHedge import Deep_Hedge as dh
+from Parameters import get_parameters
 
-seed = 10538  # seed for replicability
+seed_train = 10538  # seed for training
+seed_test = 711  # seed for testing
+params = get_parameters()
 
+SABR_train = path(params["F0"], params["alpha"], params["beta"], params["rho"], params["nu"], params["r"],
+                  params["steps"], 10000, params["T"], seed_train)
+SABR_test = path(params["F0"], params["alpha"], params["beta"], params["rho"], params["nu"], params["r"],
+                 params["steps"], 10000, params["T"], seed_test)
 
-SABR_train = path(F0, alpha, beta, rho, nu, r, steps, 10000, T, seed)
-SABR_test = path(F0, alpha, beta, rho, nu, r, steps, 1000, T, seed)
-
-SABR_EC_hedge = dh(train_pathes=SABR_train.futures_paths, ytrain=SABR_train.payoff(K=150), test_pathes=SABR_test.futures_paths, ytest=SABR_test.payoff(K=150), initial_wealth=np.mean(SABR_train.get_price(K=150, step=0)))
+SABR_EC_hedge = dh(train_pathes=SABR_train.futures_paths, ytrain=SABR_train.payoff(K=150),
+                   test_pathes=SABR_test.futures_paths, ytest=SABR_test.payoff(K=150),
+                   initial_wealth=np.mean(SABR_train.get_price(K=150, step=0)))
 SABR_EC_hedge.train()
 SABR_EC_hedge.loss_test()
 
