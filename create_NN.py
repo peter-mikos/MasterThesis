3586b8d78a4e2762cee1cd9fdc97218e77476dd5
Peter Mikos <19peter99222@gmail.com>
2024-06-10 09:57:46 +0200

    OTM

--- create_NN.py
+++ create_NN.py
@@ -33,11 +33,22 @@ SABR_EC_hedge = dh(train_pathes=SABR_train.futures_paths, other_train=[SABR_trai
 
 SABR_EC_hedge.summary("h")
 
+SABR_EC_hedge_OTM = dh(train_pathes=SABR_train.futures_paths, other_train=[SABR_train.vol_paths],
+                   ytrain=SABR_train.payoff(K=params["F0"]*1.1),
+                   test_pathes=SABR_test.futures_paths, other_test=[SABR_test.vol_paths],
+                   ytest=SABR_test.payoff(K=params["F0"]*1.1),
+                   initial_wealth=np.mean(SABR_train.get_price(K=params["F0"], step=0)), actf="tanh", drop_out=None,
+                   n=300, d=3)
+
 # uncomment if you want to retrain the model
 # SABR_EC_hedge.train(batch_size=500, epochs=20, learning_rate=0.0001)
+# SABR_EC_hedge_OTM.train(batch_size=500, epochs=20, learning_rate=0.0001, cp_path="cp_otm.weights.h5")
 
 SABR_EC_hedge.load_weights()
+SABR_EC_hedge_OTM.load_weights(cp_path="cp_otm.weights.h5")
 SABR_EC_hedge.loss_test()
+
+# Performance ATM
 SABR_test.performance(K=params["F0"])
 
 payoffs = SABR_test.payoff(K=params["F0"])
@@ -90,3 +101,49 @@ CVaRs = pd.concat([pd.DataFrame(CVaRs), pd.Series(1-quantiles).rename("Quantiles
 
 print(VaRs)
 print(CVaRs)
+
+# Performance OTM
+SABR_test.performance(K=params["F0"]*1.1)
+
+payoffs = SABR_test.payoff(K=params["F0"]*1.1)
+wealth_NN = SABR_EC_hedge_OTM.model_wealth.predict(x=SABR_EC_hedge.xtest)[:, 0]
+wealth_BS = SABR_test.wealth_BS
+wealth_SABR = SABR_test.wealth_SABR
+wealth_nothing = SABR_test.wealth_nothing
+
+fig, axs = plt.subplots(2, 2)
+axs[0, 0].hist(wealth_nothing - payoffs)
+axs[0, 0].set_title("Nothing")
+axs[0, 1].hist(wealth_NN - payoffs)
+axs[0, 1].set_title("NN")
+axs[1, 0].hist(wealth_BS - payoffs)
+axs[1, 0].set_title("BS")
+axs[1, 1].hist(wealth_SABR - payoffs)
+axs[1, 1].set_title("SABR")
+
+for ax in axs.flat:
+    ax.set(xlabel="terminal wealth - payoffs")
+
+plt.show()
+
+quantiles = np.array([0.001, 0.01, 0.025, 0.05])
+VaRs = {
+    "Nothing": np.quantile(wealth_nothing - payoffs, quantiles),
+    "NN": np.quantile(wealth_NN - payoffs, quantiles),
+    "BS": np.quantile(wealth_BS - payoffs, quantiles),
+    "SABR": np.quantile(wealth_SABR - payoffs, quantiles)
+}
+
+CVaRs = {
+    "Nothing": cvar(wealth_nothing, payoffs, VaRs["Nothing"]),
+    "NN": cvar(wealth_NN, payoffs, VaRs["NN"]),
+    "BS": cvar(wealth_BS, payoffs, VaRs["BS"]),
+    "SABR": cvar(wealth_SABR, payoffs, VaRs["SABR"])
+}
+
+VaRs = pd.concat([pd.DataFrame(VaRs), pd.Series(1-quantiles).rename("Quantiles")], axis=1).set_index("Quantiles")
+CVaRs = pd.concat([pd.DataFrame(CVaRs), pd.Series(1-quantiles).rename("Quantiles")], axis=1).set_index("Quantiles")
+
+print(VaRs)
+print(CVaRs)
+
