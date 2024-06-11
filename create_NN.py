4a8c8dc560ba593e4225fa58fbc71480ca67c3ff
Peter Mikos <19peter99222@gmail.com>
2024-06-11 09:26:19 +0200

    ITM and OTM

--- create_NN.py
+++ create_NN.py
@@ -15,7 +15,7 @@ axs[0].plot(params["data"].index, params["data"].F)
 axs[0].set(ylabel="Futures Price USD/EUR")
 
 # plot shwowing the volatility path
-axs[1].plot(params["data"].index, params["data"].Sigma*100)
+axs[1].plot(params["data"].index, params["data"].Sigma * 100)
 axs[1].set(ylabel="Volatility in %")
 plt.show()
 
@@ -31,119 +31,126 @@ SABR_EC_hedge = dh(train_pathes=SABR_train.futures_paths, other_train=[SABR_trai
                    initial_wealth=np.mean(SABR_train.get_price(K=params["F0"], step=0)), actf="tanh", drop_out=None,
                    n=300, d=3)
 
-SABR_EC_hedge.summary("h")
-
 SABR_EC_hedge_OTM = dh(train_pathes=SABR_train.futures_paths, other_train=[SABR_train.vol_paths],
-                   ytrain=SABR_train.payoff(K=params["F0"]*1.1),
-                   test_pathes=SABR_test.futures_paths, other_test=[SABR_test.vol_paths],
-                   ytest=SABR_test.payoff(K=params["F0"]*1.1),
-                   initial_wealth=np.mean(SABR_train.get_price(K=params["F0"], step=0)), actf="tanh", drop_out=None,
-                   n=300, d=3)
+                       ytrain=SABR_train.payoff(K=params["F0"] * 1.1),
+                       test_pathes=SABR_test.futures_paths, other_test=[SABR_test.vol_paths],
+                       ytest=SABR_test.payoff(K=params["F0"] * 1.1),
+                       initial_wealth=np.mean(SABR_train.get_price(K=params["F0"], step=0)), actf="tanh", drop_out=None,
+                       n=300, d=3)
+
+SABR_EC_hedge_OTM2 = dh(train_pathes=SABR_train.futures_paths, other_train=[SABR_train.vol_paths],
+                        ytrain=SABR_train.payoff(K=params["F0"] * 1.2),
+                        test_pathes=SABR_test.futures_paths, other_test=[SABR_test.vol_paths],
+                        ytest=SABR_test.payoff(K=params["F0"] * 1.2),
+                        initial_wealth=np.mean(SABR_train.get_price(K=params["F0"], step=0)), actf="tanh",
+                        drop_out=None,
+                        n=300, d=3)
+
+SABR_EC_hedge_ITM = dh(train_pathes=SABR_train.futures_paths, other_train=[SABR_train.vol_paths],
+                       ytrain=SABR_train.payoff(K=params["F0"] * 0.9),
+                       test_pathes=SABR_test.futures_paths, other_test=[SABR_test.vol_paths],
+                       ytest=SABR_test.payoff(K=params["F0"] * 0.9),
+                       initial_wealth=np.mean(SABR_train.get_price(K=params["F0"], step=0)), actf="tanh", drop_out=None,
+                       n=300, d=3)
+
+SABR_EC_hedge_ITM2 = dh(train_pathes=SABR_train.futures_paths, other_train=[SABR_train.vol_paths],
+                        ytrain=SABR_train.payoff(K=params["F0"] * 0.8),
+                        test_pathes=SABR_test.futures_paths, other_test=[SABR_test.vol_paths],
+                        ytest=SABR_test.payoff(K=params["F0"] * 0.8),
+                        initial_wealth=np.mean(SABR_train.get_price(K=params["F0"], step=0)), actf="tanh",
+                        drop_out=None,
+                        n=300, d=3)
+
+SABR_EC_hedge.summary("h")
 
 # uncomment if you want to retrain the model
 # SABR_EC_hedge.train(batch_size=500, epochs=20, learning_rate=0.0001)
 # SABR_EC_hedge_OTM.train(batch_size=500, epochs=20, learning_rate=0.0001, cp_path="cp_otm.weights.h5")
+# SABR_EC_hedge_OTM2.train(batch_size=500, epochs=20, learning_rate=0.0001, cp_path="cp_otm2.weights.h5")
+# SABR_EC_hedge_ITM.train(batch_size=500, epochs=20, learning_rate=0.0001, cp_path="cp_itm.weights.h5")
+# SABR_EC_hedge_ITM2.train(batch_size=500, epochs=20, learning_rate=0.0001, cp_path="cp_itm2.weights.h5")
 
 SABR_EC_hedge.load_weights()
 SABR_EC_hedge_OTM.load_weights(cp_path="cp_otm.weights.h5")
-SABR_EC_hedge.loss_test()
+SABR_EC_hedge_OTM2.load_weights(cp_path="cp_otm2.weights.h5")
+SABR_EC_hedge_ITM.load_weights(cp_path="cp_itm.weights.h5")
+SABR_EC_hedge_ITM2.load_weights(cp_path="cp_itm2.weights.h5")
 
-# Performance ATM
-SABR_test.performance(K=params["F0"])
+def performance_summary(NN, test_paths, strike):
+    # Loss Statistics
+    NN.loss_test()
+    test_paths.performance(K=strike)
 
-payoffs = SABR_test.payoff(K=params["F0"])
-wealth_NN = SABR_EC_hedge.model_wealth.predict(x=SABR_EC_hedge.xtest)[:, 0]
-wealth_BS = SABR_test.wealth_BS
-wealth_SABR = SABR_test.wealth_SABR
-wealth_nothing = SABR_test.wealth_nothing
+    # Terminal wealth - payoffs
+    payoffs = test_paths.payoff(K=strike)
+    wealth_NN = NN.model_wealth.predict(x=NN.xtest)[:, 0] - payoffs
+    wealth_BS = test_paths.wealth_BS - payoffs
+    wealth_SABR = test_paths.wealth_SABR - payoffs
+    wealth_nothing = test_paths.wealth_nothing - payoffs
 
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].hist(wealth_nothing - payoffs)
-axs[0, 0].set_title("Nothing")
-axs[0, 1].hist(wealth_NN - payoffs)
-axs[0, 1].set_title("NN")
-axs[1, 0].hist(wealth_BS - payoffs)
-axs[1, 0].set_title("BS")
-axs[1, 1].hist(wealth_SABR - payoffs)
-axs[1, 1].set_title("SABR")
+    # plotting wealth distributions:
+    fig, axs = plt.subplots(2, 2)
+    axs[0, 0].hist(wealth_nothing)
+    axs[0, 0].set_title("Nothing")
+    axs[0, 1].hist(wealth_NN)
+    axs[0, 1].set_title("NN")
+    axs[1, 0].hist(wealth_BS)
+    axs[1, 0].set_title("BS")
+    axs[1, 1].hist(wealth_SABR)
+    axs[1, 1].set_title("SABR")
 
-for ax in axs.flat:
-    ax.set(xlabel="terminal wealth - payoffs")
+    for ax in axs.flat:
+        ax.set(xlabel="terminal wealth - payoffs")
 
-plt.show()
+    plt.show()
 
-quantiles = np.array([0.001, 0.01, 0.025, 0.05])
-VaRs = {
-    "Nothing": np.quantile(wealth_nothing - payoffs, quantiles),
-    "NN": np.quantile(wealth_NN - payoffs, quantiles),
-    "BS": np.quantile(wealth_BS - payoffs, quantiles),
-    "SABR": np.quantile(wealth_SABR - payoffs, quantiles)
-}
+    # Risk measures
+    quantiles = np.array([0.001, 0.01, 0.025, 0.05])
+    VaRs = {
+        "Nothing": np.quantile(wealth_nothing, quantiles),
+        "NN": np.quantile(wealth_NN, quantiles),
+        "BS": np.quantile(wealth_BS, quantiles),
+        "SABR": np.quantile(wealth_SABR, quantiles)
+    }
+
+    def cvar(wealth, VaRs):
+        cvars = np.zeros(len(VaRs))
+        tw = wealth
+        for i in range(len(VaRs)):
+            cvars[i] = tw[tw < VaRs[i]].mean()
+        return cvars
+
+    CVaRs = {
+        "Nothing": cvar(wealth_nothing, VaRs["Nothing"]),
+        "NN": cvar(wealth_NN, VaRs["NN"]),
+        "BS": cvar(wealth_BS, VaRs["BS"]),
+        "SABR": cvar(wealth_SABR, VaRs["SABR"])
+    }
+
+    VaRs = pd.concat([pd.DataFrame(VaRs), pd.Series(1 - quantiles).rename("Quantiles")], axis=1).set_index("Quantiles")
+    CVaRs = pd.concat([pd.DataFrame(CVaRs), pd.Series(1 - quantiles).rename("Quantiles")], axis=1).set_index(
+        "Quantiles")
+
+    print(VaRs)
+    print(CVaRs)
 
 
-def cvar(wealth, payoff, VaRs):
-    cvars = np.zeros(len(VaRs))
-    tw = wealth - payoff
-    for i in range(len(VaRs)):
-        cvars[i] = tw[tw < VaRs[i]].mean()
-    return cvars
+# ATM
+print("ATM")
+performance_summary(SABR_EC_hedge, SABR_test, params["F0"])
 
+# OTM
+print("OTM")
+performance_summary(SABR_EC_hedge_OTM, SABR_test, params["F0"] * 1.1)
 
-CVaRs = {
-    "Nothing": cvar(wealth_nothing, payoffs, VaRs["Nothing"]),
-    "NN": cvar(wealth_NN, payoffs, VaRs["NN"]),
-    "BS": cvar(wealth_BS, payoffs, VaRs["BS"]),
-    "SABR": cvar(wealth_SABR, payoffs, VaRs["SABR"])
-}
+# OTM 2
+print("OTM 2")
+performance_summary(SABR_EC_hedge_OTM2, SABR_test, params["F0"] * 1.2)
 
-VaRs = pd.concat([pd.DataFrame(VaRs), pd.Series(1-quantiles).rename("Quantiles")], axis=1).set_index("Quantiles")
-CVaRs = pd.concat([pd.DataFrame(CVaRs), pd.Series(1-quantiles).rename("Quantiles")], axis=1).set_index("Quantiles")
+# ITM
+print("ITM")
+performance_summary(SABR_EC_hedge_ITM, SABR_test, params["F0"] * 0.9)
 
-print(VaRs)
-print(CVaRs)
-
-# Performance OTM
-SABR_test.performance(K=params["F0"]*1.1)
-
-payoffs = SABR_test.payoff(K=params["F0"]*1.1)
-wealth_NN = SABR_EC_hedge_OTM.model_wealth.predict(x=SABR_EC_hedge.xtest)[:, 0]
-wealth_BS = SABR_test.wealth_BS
-wealth_SABR = SABR_test.wealth_SABR
-wealth_nothing = SABR_test.wealth_nothing
-
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].hist(wealth_nothing - payoffs)
-axs[0, 0].set_title("Nothing")
-axs[0, 1].hist(wealth_NN - payoffs)
-axs[0, 1].set_title("NN")
-axs[1, 0].hist(wealth_BS - payoffs)
-axs[1, 0].set_title("BS")
-axs[1, 1].hist(wealth_SABR - payoffs)
-axs[1, 1].set_title("SABR")
-
-for ax in axs.flat:
-    ax.set(xlabel="terminal wealth - payoffs")
-
-plt.show()
-
-quantiles = np.array([0.001, 0.01, 0.025, 0.05])
-VaRs = {
-    "Nothing": np.quantile(wealth_nothing - payoffs, quantiles),
-    "NN": np.quantile(wealth_NN - payoffs, quantiles),
-    "BS": np.quantile(wealth_BS - payoffs, quantiles),
-    "SABR": np.quantile(wealth_SABR - payoffs, quantiles)
-}
-
-CVaRs = {
-    "Nothing": cvar(wealth_nothing, payoffs, VaRs["Nothing"]),
-    "NN": cvar(wealth_NN, payoffs, VaRs["NN"]),
-    "BS": cvar(wealth_BS, payoffs, VaRs["BS"]),
-    "SABR": cvar(wealth_SABR, payoffs, VaRs["SABR"])
-}
-
-VaRs = pd.concat([pd.DataFrame(VaRs), pd.Series(1-quantiles).rename("Quantiles")], axis=1).set_index("Quantiles")
-CVaRs = pd.concat([pd.DataFrame(CVaRs), pd.Series(1-quantiles).rename("Quantiles")], axis=1).set_index("Quantiles")
-
-print(VaRs)
-print(CVaRs)
-
+# ITM 2
+print("ITM 2")
+performance_summary(SABR_EC_hedge_ITM2, SABR_test, params["F0"] * 0.8)
