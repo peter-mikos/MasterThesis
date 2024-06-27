533fa8f63ca6778ca88664961743d83cb16f2fa3
Peter Mikos <19peter99222@gmail.com>
2024-06-27 10:01:04 +0200

    Multiple Currencies Setup

--- Parameters.py
+++ Parameters.py
@@ -14,7 +14,7 @@ def get_parameters(r_tar, r_base, zips, files):
         return 1 / ((1 + r) ** YF)
 
     # Read in tick-data from zip file
-    eurusd = pd.concat(
+    basetar = pd.concat(
         [
             pd.read_csv(zipfile.ZipFile("data/" + zips[0]).open(files[0]),
                         header=None, delimiter=";"),
@@ -25,37 +25,38 @@ def get_parameters(r_tar, r_base, zips, files):
     )
 
     # Data transformation:
-    eurusd.columns = ["DateTime", "Open", "High", "Low", "Close", "Volume"]
-    eurusd.DateTime = eurusd.DateTime.apply(lambda x: dt.datetime.strptime(x, '%Y%m%d %H%M%S'))
-    eurusd = eurusd.set_index(eurusd.DateTime)
+    basetar.columns = ["DateTime", "Open", "High", "Low", "Close", "Volume"]
+    basetar.DateTime = basetar.DateTime.apply(lambda x: dt.datetime.strptime(x, '%Y%m%d %H%M%S'))
+    ind = (basetar.DateTime > dt.datetime(2023, 1, 2, 0, 0)) & (basetar.DateTime < dt.datetime(2024, 1, 2, 23, 59))
+    basetar = basetar.set_index(basetar.DateTime)
 
     # forward data
-    eurusd = eurusd.loc[dt.datetime(2023, 1, 2, 0, 0):dt.datetime(2024, 1, 2, 23, 59), "DateTime":"Close"]
-    eurusd["Date"] = eurusd.DateTime.apply(lambda x: x.date())
-    eurusd["YF"] = eurusd.DateTime.apply(lambda x: year_frac(x, dt.datetime(2024, 1, 2, 23, 59)))  # year fraction
-    eurusd["D_EUR"] = eurusd.YF.apply(lambda x: discount_factor(x, r_base))
-    eurusd["D_USD"] = eurusd.YF.apply(lambda x: discount_factor(x, r_tar))
+    basetar = basetar.loc[ind, "DateTime":"Close"]
+    basetar["Date"] = basetar.DateTime.apply(lambda x: x.date())
+    basetar["YF"] = basetar.DateTime.apply(lambda x: year_frac(x, dt.datetime(2024, 1, 2, 23, 59)))  # year fraction
+    basetar["D_EUR"] = basetar.YF.apply(lambda x: discount_factor(x, r_base))
+    basetar["D_USD"] = basetar.YF.apply(lambda x: discount_factor(x, r_tar))
     # EUR is the base currency and USD the target currency so the forward price is:
     # Forward = Spot * D_USD / D_EUR
-    eurusd["Close_Forward"] = eurusd.Close * eurusd.D_USD / eurusd.D_EUR
-    eurusd_daily = pd.concat(
+    basetar["Close_Forward"] = basetar.Close * basetar.D_USD / basetar.D_EUR
+    basetar_daily = pd.concat(
         [
-            eurusd.groupby(["Date"])["Close_Forward"].apply(lambda x: x[-1]),
-            eurusd.groupby(["Date"])["Close_Forward"].apply(lambda x: x.std() * np.sqrt(24 * 60)),
-            eurusd.groupby(["Date"])["YF"].apply(lambda x: 1 - x.max())
+            basetar.groupby(["Date"])["Close_Forward"].apply(lambda x: x[-1]),
+            basetar.groupby(["Date"])["Close_Forward"].apply(lambda x: x.std() * np.sqrt(24 * 60)),
+            basetar.groupby(["Date"])["YF"].apply(lambda x: 1 - x.max())
         ],
         axis=1
     )
-    eurusd_daily.columns = ["F", "Sigma", "t"]
+    basetar_daily.columns = ["F", "Sigma", "t"]
 
-    eurusd_daily["Sigma"] = eurusd_daily["Sigma"] / eurusd_daily.reset_index().set_index("Date", drop=False)[
+    basetar_daily["Sigma"] = basetar_daily["Sigma"] / basetar_daily.reset_index().set_index("Date", drop=False)[
         "Date"].diff().transform(lambda x: np.sqrt(x.days)).fillna(1)
 
-    F0 = eurusd_daily.F[0]  # initial futures value
-    alpha = eurusd_daily.Sigma[0]  # initial volatility value
+    F0 = basetar_daily.F[0]  # initial futures value
+    alpha = basetar_daily.Sigma[0]  # initial volatility value
     beta = 1  # shape parameter
-    rho = eurusd_daily[["F", "Sigma"]].corr().loc["F", "Sigma"]  # correlation between BMs
-    nu = eurusd_daily.Sigma.std()  # volvol
+    rho = basetar_daily[["F", "Sigma"]].corr().loc["F", "Sigma"]  # correlation between BMs
+    nu = basetar_daily.Sigma.std()  # volvol
     # r = (eurusd.D_EUR[0] / eurusd.D_USD[0]) - 1  # interest rate
     steps = 365  # number of time steps
     T = 1  # time of maturity
@@ -70,5 +71,5 @@ def get_parameters(r_tar, r_base, zips, files):
         "r_base": r_base,
         "steps": steps,
         "T": T,
-        "data": eurusd_daily
+        "data": basetar_daily
     }
