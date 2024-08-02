f9e25ddb890de83d177813862ed6d85b991da0e0
Peter Mikos <19peter99222@gmail.com>
2024-08-02 15:18:54 +0200

    added means to metrics

--- print_tables.py
+++ print_tables.py
@@ -10,7 +10,7 @@ for CCY in performance.keys():
         table = pd.DataFrame(
             np.array([
                 "Loss on real Path",
-                "Test Loss",
+                "Mean",
                 "Standard Error",
                 "99.9%-VaR",
                 "99%-VaR",
@@ -25,29 +25,54 @@ for CCY in performance.keys():
         )
         for name in ["Nothing", "BS", "SABR"]:
             table[name] = pd.Series(np.array([
-                performance[CCY][moneyness]["Real_Loss"][name][0],
-                performance[CCY][moneyness]["Loss"][name],
+                -performance[CCY][moneyness]["Real_Loss"][name][0],
+                -performance[CCY][moneyness]["Mean"][name],
                 performance[CCY][moneyness]["Standard Error"][name],
-                performance[CCY][moneyness]["VaR"][name].iloc[0],
-                performance[CCY][moneyness]["VaR"][name].iloc[1],
-                performance[CCY][moneyness]["VaR"][name].iloc[2],
-                performance[CCY][moneyness]["VaR"][name].iloc[3],
-                performance[CCY][moneyness]["CVaR"][name].iloc[0],
-                performance[CCY][moneyness]["CVaR"][name].iloc[1],
-                performance[CCY][moneyness]["CVaR"][name].iloc[2],
-                performance[CCY][moneyness]["CVaR"][name].iloc[3],
+                -performance[CCY][moneyness]["VaR"][name].iloc[0],
+                -performance[CCY][moneyness]["VaR"][name].iloc[1],
+                -performance[CCY][moneyness]["VaR"][name].iloc[2],
+                -performance[CCY][moneyness]["VaR"][name].iloc[3],
+                -performance[CCY][moneyness]["CVaR"][name].iloc[0],
+                -performance[CCY][moneyness]["CVaR"][name].iloc[1],
+                -performance[CCY][moneyness]["CVaR"][name].iloc[2],
+                -performance[CCY][moneyness]["CVaR"][name].iloc[3],
             ]))
         table["NN"] = pd.Series(np.array([
-            performance[CCY][moneyness]["Real_Loss"]["NN"][0,0],
-            performance[CCY][moneyness]["Loss"]["NN"],
+            -performance[CCY][moneyness]["Real_Loss"]["NN"][0, 0],
+            -performance[CCY][moneyness]["Mean"]["NN"],
             performance[CCY][moneyness]["Standard Error"]["NN"],
-            performance[CCY][moneyness]["VaR"]["NN"].iloc[0],
-            performance[CCY][moneyness]["VaR"]["NN"].iloc[1],
-            performance[CCY][moneyness]["VaR"]["NN"].iloc[2],
-            performance[CCY][moneyness]["VaR"]["NN"].iloc[3],
-            performance[CCY][moneyness]["CVaR"]["NN"].iloc[0],
-            performance[CCY][moneyness]["CVaR"]["NN"].iloc[1],
-            performance[CCY][moneyness]["CVaR"]["NN"].iloc[2],
-            performance[CCY][moneyness]["CVaR"]["NN"].iloc[3],
+            -performance[CCY][moneyness]["VaR"]["NN"].iloc[0],
+            -performance[CCY][moneyness]["VaR"]["NN"].iloc[1],
+            -performance[CCY][moneyness]["VaR"]["NN"].iloc[2],
+            -performance[CCY][moneyness]["VaR"]["NN"].iloc[3],
+            -performance[CCY][moneyness]["CVaR"]["NN"].iloc[0],
+            -performance[CCY][moneyness]["CVaR"]["NN"].iloc[1],
+            -performance[CCY][moneyness]["CVaR"]["NN"].iloc[2],
+            -performance[CCY][moneyness]["CVaR"]["NN"].iloc[3],
         ]))
-        print(table.to_latex(index=False).replace("%", "\%").replace("99.9\%-VaR", "\midrule\n99.9\%-VaR").replace("99.9\%-CVaR", "\midrule\n99.9\%-CVaR"))
+        tabular = table.to_latex(index=False).replace("%", "\%").replace("99.9\%-VaR", "\midrule\n99.9\%-VaR").replace(
+            "99.9\%-CVaR", "\midrule\n99.9\%-CVaR")
+        mon = None
+        mon_mult = 1
+        if moneyness == "OTM2":
+            mon = " out of the money "
+            mon_mult = 1.4
+        elif moneyness == "OTM":
+            mon = " out of the money "
+            mon_mult = 1.2
+        elif moneyness == "ATM":
+            mon = " at the money "
+        elif moneyness == "ITM2":
+            mon = " in the money "
+            mon_mult = 0.8
+        elif moneyness == "ITM":
+            mon = " in the money "
+            mon_mult = 0.6
+        caption = ("Risk Metrics of the loss distributions from hedging " + CCY +
+                   mon + "call options with initial underlying price: " +
+                   str(round(params[CCY]["F0"], 4)) + " and strike price: " +
+                   str(round(params[CCY]["F0"] * mon_mult, 4)) + ', using the hedging strategies "Nothing"-strategy (Nothing), Black Scholes delta hedging (BS), approximate SABR delta hedging (SABR) and deep hedging (NN).')
+        caption = "\n\\caption{" + caption + "}\n"
+        label = "\\label{Tab:" + CCY + "_" + moneyness + "}"
+        table_latex = "\\begin{table}[h!]\n\\centering\n" + tabular + caption + label + "\n\\end{table}\n\n"
+        print(table_latex)
